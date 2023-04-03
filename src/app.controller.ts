08ec15a24121da142a785d4c138990be84b146ef
leixianrong <553374716@qq.com>
2023-04-03 16:49:07 +0800

    v1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello()
   }
-
-  @Get("hello/:name")
-  getHelloName(@Param("name") name: string): string {
-    return this.appService.getHelloName(name)
-  }
 }
